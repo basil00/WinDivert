130e25c05b7c5b2ff435cf19ad95dfd363d29c28
basil00 <basil@reqrypt.org>
2015-07-17 19:13:55 +0800

    Bump copyright year.

--- examples/passthru/passthru.c
+++ examples/passthru/passthru.c
@@ -1,6 +1,6 @@
 /*
  * passthru.c
- * (C) 2014, all rights reserved,
+ * (C) 2015, all rights reserved,
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
