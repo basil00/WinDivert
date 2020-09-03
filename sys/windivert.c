134dd37bd0a87d3d236214e42edc1867b8911c6f
basil00 <basil@reqrypt.org>
2020-09-03 08:12:39 +0800

    Insert all WinDivert sublayers at the max weight.

--- sys/windivert.c
+++ sys/windivert.c
@@ -690,7 +690,7 @@ static const struct layer_s windivert_layer_resource_assignment_ipv4 =
     &WINDIVERT_SUBLAYER_RESOURCE_ASSIGNMENT_IPV4_GUID,
     windivert_resource_assignment_v4_classify,
     NULL,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_RESOURCE_ASSIGNMENT_IPV4                            \
     (&windivert_layer_resource_assignment_ipv4)
@@ -707,7 +707,7 @@ static const struct layer_s windivert_layer_resource_assignment_ipv6 =
     &WINDIVERT_SUBLAYER_RESOURCE_ASSIGNMENT_IPV6_GUID,
     windivert_resource_assignment_v6_classify,
     NULL,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_RESOURCE_ASSIGNMENT_IPV6                            \
     (&windivert_layer_resource_assignment_ipv6)
@@ -724,7 +724,7 @@ static const struct layer_s windivert_layer_resource_release_ipv4 =
     &WINDIVERT_SUBLAYER_RESOURCE_RELEASE_IPV4_GUID,
     windivert_resource_release_v4_classify,
     NULL,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_RESOURCE_RELEASE_IPV4                              \
     (&windivert_layer_resource_release_ipv4)
@@ -741,7 +741,7 @@ static const struct layer_s windivert_layer_resource_release_ipv6 =
     &WINDIVERT_SUBLAYER_RESOURCE_RELEASE_IPV6_GUID,
     windivert_resource_release_v6_classify,
     NULL,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_RESOURCE_RELEASE_IPV6                              \
     (&windivert_layer_resource_release_ipv6)
@@ -758,7 +758,7 @@ static const struct layer_s windivert_layer_auth_connect_ipv4 =
     &WINDIVERT_SUBLAYER_AUTH_CONNECT_IPV4_GUID,
     windivert_auth_connect_v4_classify,
     NULL,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_AUTH_CONNECT_IPV4                                   \
     (&windivert_layer_auth_connect_ipv4)
@@ -775,7 +775,7 @@ static const struct layer_s windivert_layer_auth_connect_ipv6 =
     &WINDIVERT_SUBLAYER_AUTH_CONNECT_IPV6_GUID,
     windivert_auth_connect_v6_classify,
     NULL,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_AUTH_CONNECT_IPV6                                   \
     (&windivert_layer_auth_connect_ipv6)
@@ -792,7 +792,7 @@ static const struct layer_s windivert_layer_endpoint_closure_ipv4 =
     &WINDIVERT_SUBLAYER_ENDPOINT_CLOSURE_IPV4_GUID,
     windivert_endpoint_closure_v4_classify,
     NULL,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_ENDPOINT_CLOSURE_IPV4                               \
     (&windivert_layer_endpoint_closure_ipv4)
@@ -809,7 +809,7 @@ static const struct layer_s windivert_layer_endpoint_closure_ipv6 =
     &WINDIVERT_SUBLAYER_ENDPOINT_CLOSURE_IPV6_GUID,
     windivert_endpoint_closure_v6_classify,
     NULL,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_ENDPOINT_CLOSURE_IPV6                               \
     (&windivert_layer_endpoint_closure_ipv6)
@@ -826,7 +826,7 @@ static const struct layer_s windivert_layer_auth_listen_ipv4 =
     &WINDIVERT_SUBLAYER_AUTH_LISTEN_IPV4_GUID,
     windivert_auth_listen_v4_classify,
     NULL,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_AUTH_LISTEN_IPV4                                    \
     (&windivert_layer_auth_listen_ipv4)
@@ -843,7 +843,7 @@ static const struct layer_s windivert_layer_auth_listen_ipv6 =
     &WINDIVERT_SUBLAYER_AUTH_LISTEN_IPV6_GUID,
     windivert_auth_listen_v6_classify,
     NULL,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_AUTH_LISTEN_IPV6                                    \
     (&windivert_layer_auth_listen_ipv6)
@@ -860,7 +860,7 @@ static const struct layer_s windivert_layer_auth_recv_accept_ipv4 =
     &WINDIVERT_SUBLAYER_AUTH_RECV_ACCEPT_IPV4_GUID,
     windivert_auth_recv_accept_v4_classify,
     NULL,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_AUTH_RECV_ACCEPT_IPV4                               \
     (&windivert_layer_auth_recv_accept_ipv4)
@@ -877,7 +877,7 @@ static const struct layer_s windivert_layer_auth_recv_accept_ipv6 =
     &WINDIVERT_SUBLAYER_AUTH_RECV_ACCEPT_IPV6_GUID,
     windivert_auth_recv_accept_v6_classify,
     NULL,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_AUTH_RECV_ACCEPT_IPV6                               \
     (&windivert_layer_auth_recv_accept_ipv6)
@@ -894,7 +894,7 @@ static const struct layer_s windivert_layer_flow_established_ipv4 =
     &WINDIVERT_SUBLAYER_FLOW_ESTABLISHED_IPV4_GUID,
     windivert_flow_established_v4_classify,
     windivert_flow_delete_notify,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_FLOW_ESTABLISHED_IPV4                               \
     (&windivert_layer_flow_established_ipv4)
@@ -911,7 +911,7 @@ static const struct layer_s windivert_layer_flow_established_ipv6 =
     &WINDIVERT_SUBLAYER_FLOW_ESTABLISHED_IPV6_GUID,
     windivert_flow_established_v6_classify,
     windivert_flow_delete_notify,
-    0
+    UINT16_MAX
 };
 #define WINDIVERT_LAYER_FLOW_ESTABLISHED_IPV6                               \
     (&windivert_layer_flow_established_ipv6)
